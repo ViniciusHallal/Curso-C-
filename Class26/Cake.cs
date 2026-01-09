7acb9ab07ddaa97569e088f6e267ccd6fd7ee704
ViniciusHallal <Vinicius.vmhallal@gmail.com>
2026-01-08 22:14:04 -0300

    Class26 subindo

---
+++ Class26/Cake.cs
@@ -0,0 +1,29 @@
+﻿namespace Class27
+{
+    public class Cake
+    {
+        public string Flavor;
+        public string Filling;
+        public string Coverage;
+        public int Layers;
+        public string Size;
+
+        public Cake()
+        {
+            Flavor = "Chocolate";
+            Filling = "Coconut";
+            Coverage = "Chocolate";
+            Layers = 3;
+            Size = "Medium";
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"Flavor: {Flavor}");
+            Console.WriteLine($"Filling: {Filling}");
+            Console.WriteLine($"Coverage: {Coverage}");
+            Console.WriteLine($"Layers: {Layers}");
+            Console.WriteLine($"Size: {Size}");
+        }
+    }
+}
---
+++ Class26/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Class27;
+
+public class Program
+{
+    public static void Main()
+    {
+        Cake cake = new Cake();
+        cake.Show();
+    }
+}
